1c64a8bc1739397840a22cd3e5bfe2bf28bad4be
Loganathan C <loganathanchandrasekar@objectways.com>
2025-01-06 18:13:03 +0530

    Create nodemailer.js

---
+++ config/nodemailer.js
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer'
+const transporter = nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_USER_PASSWORD
+    }
+});
+
+export default transporter;
