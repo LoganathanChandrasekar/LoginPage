60bdcb40688c77a3b85293538276e69a8084b7ad
Loganathan C <loganathanchandrasekar@objectways.com>
2025-01-06 18:17:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
 import connectDb from './config/mongodb.js'
+import authRouter from './routes/authRouters.js'
 const app = express();
 const port=process.env.PORT || 4000;
 connectDb()
@@ -13,6 +14,7 @@ app.use(cors({credentials: true}));
 app.get('/',(req,res)=>{
     res.send('API Working')
 })
+app.use('/api/auth',authRouter)
 app.listen(port,()=>{
     console.log(`Server Running on ........http://127.0.0.1:${port}`);
 })
