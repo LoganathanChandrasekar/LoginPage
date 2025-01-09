6ab9925af3927c1d34d13819b38aad7e5cbc9a8f
Loganathan <loganathanchandrasekar@objectways.com>
2025-01-09 23:16:55 +0530

    axios stage

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContent = createContext()
 
 export const AppContextProvider =(props)=>{
+    const backendUrl = import.meta.env.VITE_BACKEND
+    const [isLoggedIn,setIsLoggedIn] = useState(false)
+    const [userData,setUserData] = useState(false)
     const value ={
-        
+        backendUrl,
+        isLoggedIn,setIsLoggedIn,
+        userData,setUserData
+
     }
     return(
-            <AppContent.Provider> 
+            <AppContent.Provider value={value}> 
                 {props.children}
             </AppContent.Provider>
     )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,36 @@
-import React ,{useState} from 'react'
+import React ,{useContext, useState} from 'react'
 import {useNavigate} from 'react-router-dom';
 import { assets } from '../assets/assets';
+import { AppContent } from '../context/AppContext';
+import axios from 'axios'
 const Login = () => {
     const navigate = useNavigate()
+    const {backendUrl,setIsLoggedIn}=useContext(AppContent)
+
     const  [state, setState] = useState('Sign up');
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+
+    const onSubmitHandler =async (e)=>{
+      try{
+        e.preventDefault()
+        if(state ==='Sign up'){
+          const {data}=await axios.post(backendUrl + '/api/auth/register',{name,password,email})
+        }else{
+
+        }
+      }catch(error){
+
+      }
+    }
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
       <img onClick={()=>navigate('/')} src={assets.logo} alt ="" className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"/>
       <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
         <h2 className="text-3xl font-semibold text-white text-center mb-3">{state ==='Sign up'?'Create account':'Login'}</h2>
         <p className="text-center text-sm mb-3">{state ==='Sign up'?'Create your account':'Login to your account!'}</p>
-        <form>
+        <form onClick={onSubmitHandler}>
           {state === 'Sign up' && (
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
             <img src={assets.person_icon} alt=""/>
